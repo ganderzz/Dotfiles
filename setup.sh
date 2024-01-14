800cf20c8e19846d5bd66b3d73d26dcf204d7917
Dylan Paulus <dylantpaulus@gmail.com>
2024-01-14 10:42:38 -0800

    setup alacritty config

--- setup.sh
+++ setup.sh
@@ -43,11 +43,11 @@ printf "${GREEN}Updating PATH...${NC}\n"
 echo -e "\nexport PATH=${SCRIPT_DIR}:\$PATH" >> ~/.zshrc
 
 cp "${SCRIPT_DIR}/starship.toml" ~/.config/starship.toml
+mkdir -p ~/.config/alacritty
+cp "${SCRIPT_DIR}/.alacritty.yml" ~/.config/alacritty/alacritty.yml
 
 printf "${GREEN}Copying over nvim config...${NC}\n"
 rsync -aqz "${SCRIPT_DIR}/nvim" ~/.config/nvim/
 
-source ~/.zshrc
-
-printf "${GREEN}-------- [ COMPLETE ] --------${NC}\n"
+printf "\n${GREEN}-------- [ COMPLETE ] --------${NC}\n"
 printf "${GREEN}Bye 👋🏻${NC}\n"
